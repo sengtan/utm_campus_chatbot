a06da937c598f046fb383cca433908d122decc16
sengtan <tansenghooi1998@gmail.com>
2025-06-21 22:21:17 +0800

    Move to local host

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 from app import app
-import routes
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
